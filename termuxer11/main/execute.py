1fb89abbe99a1a11560f06044a86d4958e2a267e
nutchellxer-ux <nutchellxer@gmail.com>
2026-03-10 20:28:22 +0800

    Update execute.py

--- termuxer11/main/execute.py
+++ termuxer11/main/execute.py
@@ -3,11 +3,11 @@ import sys
 import os
 
 def run_pkg(pkg_name):
-    # Look for the file in the parent directory
+    # Look for the package in the parent directory
     target_path = f"../{pkg_name}.py"
     
     if os.path.exists(target_path):
-        print(f"[*] Executing {pkg_name}...")
+        print(f"\033[92m[*] Launching {pkg_name}...\033[0m")
         subprocess.run([sys.executable, target_path])
     else:
-        print(f"[!] Error: {pkg_name}.py is not installed in the root.") 
+        print(f"\033[91m[!] Error: {pkg_name}.py is not installed.\033[0m")
